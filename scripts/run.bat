99d1b457687b4632d665aeb698612a93806ae9ab
Emre Kovanci <kovanciemre@gmail.com>
2025-12-07 12:33:30 +0300

    sync

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,5 +1,6 @@
 @echo off
 
+:: Configs
 set APP_PRESET_NAME=windows-ninja-clangcl
 set APP_BUILD_TYPE=Debug
 set APP_ARCH=x64
@@ -7,27 +8,41 @@ set APP_NAME=Application.exe
 set MSVC_VERSION=18
 
 set NO_CONFIGURE=0
-if "%1"=="-no_configure" set NO_CONFIGURE=1
+if "%1"=="-no-configure" set NO_CONFIGURE=1
 
+:: Paths
 set CURRENT_DIR=%~dp0
 for %%a in ("%CURRENT_DIR%\..") do set PROJECT_DIR=%%~fa
 
-:: vcvarsall call to initialize environment for x64
+set BUILD_DIR=%PROJECT_DIR%\build\%APP_PRESET_NAME%
+set BUILD_APP_DIR=%BUILD_DIR%\app
+set BUILD_COMPILE_COMMANDS=%BUILD_DIR%\compile_commands.json
+set TARGET_COMPILE_COMMANDS=%PROJECT_DIR%\compile_commands.json
+
+echo ------------------------------------------------------
+echo [run.bat] Project Dir:             %PROJECT_DIR%
+echo [run.bat] Build Dir:               %BUILD_DIR%
+echo [run.bat] Compile Commands Src:    %BUILD_COMPILE_COMMANDS%
+echo [run.bat] Compile Commands Dst:    %TARGET_COMPILE_COMMANDS%
+echo ------------------------------------------------------
+
+:: Initialize MSVC environment
 call "%ProgramFiles%\Microsoft Visual Studio\%MSVC_VERSION%\Community\VC\Auxiliary\Build\vcvarsall.bat" %APP_ARCH%
 
-:: cmake configure
+:: CMake Configure
 if %NO_CONFIGURE%==0 (
     cmake --preset="%APP_PRESET_NAME%"
-    if errorlevel 1 goto :end
-) else (
-    echo [run.bat] Skipping CMake configure due to -no_configure
+    if errorlevel 1 exit /b
 )
 
-:: cmake build
+:: CMake Build
 cmake --build --preset="%APP_PRESET_NAME%" --config="%APP_BUILD_TYPE%"
-if errorlevel 1 goto :end
+if errorlevel 1 exit /b
 
-:: run the app
-start "" "%PROJECT_DIR%\build\%APP_PRESET_NAME%\app\%APP_NAME%"
+:: Copy compile_commands.json
+if exist "%TARGET_COMPILE_COMMANDS%" del "%TARGET_COMPILE_COMMANDS%"
+copy /y "%BUILD_COMPILE_COMMANDS%" "%TARGET_COMPILE_COMMANDS%" >nul
 
-:end
+:: Run Application
+set APP_PATH=%BUILD_APP_DIR%\%APP_NAME%
+start "" "%APP_PATH%"
